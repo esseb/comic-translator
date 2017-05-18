c8c1f5c903d44d2b1e6ad5d0a2411f31835ac31a
Stein Cato Blostrupmoen <esseb@esseb.com>
2017-05-18 23:30:57 +0200

    Make comic page background color fill the whole screen

--- pages/comic.js
+++ pages/comic.js
@@ -70,77 +70,86 @@ class Comic extends Component {
           className="comic-page"
           style={{ backgroundColor: comic.backgroundColor }}
         >
-          <h1>{comic.title}</h1>
-          <a href={comic.url} target="_blank" rel="noopener">{comic.url}</a>
+          <div className="comic-page__content">
+            <h1 className="comic-page__heading">{comic.title}</h1>
+            <a
+              className="comic-page__link"
+              href={comic.url}
+              target="_blank"
+              rel="noopener"
+            >
+              {comic.url}
+            </a>
 
-          {panels.map((panel, index) => (
-            <div key={index} className="comic-page__panel">
-              {panel.bubbles.map((bubble, index) => (
-                <div key={index} className="comic-page__bubble">
-                  <TranslateBubble
-                    text={bubble.text}
-                    onSelect={this.handledSelectText}
-                  />
-                </div>
-              ))}
-            </div>
-          ))}
+            {panels.map((panel, index) => (
+              <div key={index} className="comic-page__panel">
+                {panel.bubbles.map((bubble, index) => (
+                  <div key={index} className="comic-page__bubble">
+                    <TranslateBubble
+                      text={bubble.text}
+                      onSelect={this.handledSelectText}
+                    />
+                  </div>
+                ))}
+              </div>
+            ))}
 
-          <ReactModal
-            isOpen={Boolean(this.state.translatedText)}
-            onRequestClose={this.closeDialog}
-            contentLabel="Translation"
-            style={{
-              overlay: {
-                backgroundColor: "rgba(0, 0, 0, 0.75)",
-                bottom: 0,
-                left: 0,
-                position: "fixed",
-                right: 0,
-                top: 0
-              },
-              content: {
-                background: "#fff",
-                border: 0,
-                borderRadius: "5px",
-                bottom: "4vw",
-                fontFamily: "Courier, monospace",
-                left: "4vw",
-                outline: "none",
-                overflow: "auto",
-                padding: "40px 30px 30px 30px",
-                position: "absolute",
-                right: "4vw",
-                top: "4vw",
-                WebkitOverflowScrolling: "touch"
-              }
-            }}
-          >
-            <button onClick={this.closeDialog} aria-label="close">X</button>
+            <ReactModal
+              isOpen={Boolean(this.state.translatedText)}
+              onRequestClose={this.closeDialog}
+              contentLabel="Translation"
+              style={{
+                overlay: {
+                  backgroundColor: "rgba(0, 0, 0, 0.75)",
+                  bottom: 0,
+                  left: 0,
+                  position: "fixed",
+                  right: 0,
+                  top: 0
+                },
+                content: {
+                  background: "#fff",
+                  border: 0,
+                  borderRadius: "5px",
+                  bottom: "4vw",
+                  fontFamily: "Courier, monospace",
+                  left: "4vw",
+                  outline: "none",
+                  overflow: "auto",
+                  padding: "40px 30px 30px 30px",
+                  position: "absolute",
+                  right: "4vw",
+                  top: "4vw",
+                  WebkitOverflowScrolling: "touch"
+                }
+              }}
+            >
+              <button onClick={this.closeDialog} aria-label="close">X</button>
 
-            <h2>Translation</h2>
-            <p>{this.state.translatedText}</p>
+              <h2>Translation</h2>
+              <p>{this.state.translatedText}</p>
 
-            <h2>Original text</h2>
-            <p>{this.state.originalText}</p>
-          </ReactModal>
+              <h2>Original text</h2>
+              <p>{this.state.originalText}</p>
+            </ReactModal>
+          </div>
         </div>
 
         <style global jsx>{`
-          .comic-page {
+          .comic-page__content {
             margin: 0 auto;
             max-width: 600px;
             padding: ${baseline}px 20px;
           }
 
-          .comic-page > h1 {
+          .comic-page__heading {
             font-size: 26px;
             font-weight: bold;
             line-height: 30px;
             margin-bottom: ${baseline}px;
           }
 
-          .comic-page > a {
+          .comic-page__link {
             overflow-wrap: break-word;
             word-wrap: break-word;
           }
