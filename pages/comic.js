c4f7faa9d3672e7c067575a117cc3bba23b86928
Stein Cato Blostrupmoen <esseb@esseb.com>
2017-05-18 20:18:02 +0200

    Add translate API using MSTranslator
    Requires an API key

--- pages/comic.js
+++ pages/comic.js
@@ -1,57 +1,140 @@
 // @flow
 
+import { Component } from "react";
+import ReactModal from "react-modal";
+import fetch from "isomorphic-unfetch";
 import Main from "../layout/Main";
 import TranslateBubble from "../components/TranslateBubble";
 import { baseline } from "../variables/spacing";
 import comics from "../static/comics.json";
 
-const comic = comics[0];
-const panels = comic.pages[0].panels;
-
-const translate = text => {
-  window.open(`https://translate.google.no/m/translate#fr/en/${text}`);
+type State = {
+  originalText: null | string,
+  translatedText: null | string
 };
 
-const Comic = () => (
-  <Main title={comic.title}>
-    <div className="comic-page">
-      <h1>{comic.title}</h1>
-      <a href={comic.url} target="_blank" rel="noopener">{comic.url}</a>
+class Comic extends Component {
+  state: State;
+  handledSelectText: Function;
+  closeDialog: Function;
 
-      {panels.map((panel, index) => (
-        <div key={index} className="comic-page__panel">
-          {panel.bubbles.map((bubble, index) => (
-            <div key={index} className="comic-page__bubble">
-              <TranslateBubble text={bubble.text} onSelect={translate} />
+  constructor(props: Object) {
+    super(props);
+
+    this.state = {
+      originalText: null,
+      translatedText: null
+    };
+
+    this.handledSelectText = this.handledSelectText.bind(this);
+    this.closeDialog = this.closeDialog.bind(this);
+  }
+
+  handledSelectText(text: string) {
+    fetch(`/api/translate/${text}`)
+      .then(response => response.json())
+      .then(response => {
+        this.setState({
+          originalText: text,
+          translatedText: response.translation
+        });
+      });
+  }
+
+  closeDialog() {
+    this.setState({
+      originalText: null,
+      translatedText: null
+    });
+  }
+
+  render() {
+    const comic = comics[0];
+    const panels = comic.pages[0].panels;
+
+    return (
+      <Main title={comic.title}>
+        <div className="comic-page">
+          <h1>{comic.title}</h1>
+          <a href={comic.url} target="_blank" rel="noopener">{comic.url}</a>
+
+          {panels.map((panel, index) => (
+            <div key={index} className="comic-page__panel">
+              {panel.bubbles.map((bubble, index) => (
+                <div key={index} className="comic-page__bubble">
+                  <TranslateBubble
+                    text={bubble.text}
+                    onSelect={this.handledSelectText}
+                  />
+                </div>
+              ))}
             </div>
           ))}
+
+          <ReactModal
+            isOpen={Boolean(this.state.translatedText)}
+            onRequestClose={this.closeDialog}
+            contentLabel="Translation"
+            style={{
+              overlay: {
+                backgroundColor: "rgba(0, 0, 0, 0.75)",
+                bottom: 0,
+                left: 0,
+                position: "fixed",
+                right: 0,
+                top: 0
+              },
+              content: {
+                background: "#fff",
+                border: 0,
+                borderRadius: "5px",
+                bottom: "4vw",
+                left: "4vw",
+                outline: "none",
+                overflow: "auto",
+                padding: "40px 30px 30px 30px",
+                position: "absolute",
+                right: "4vw",
+                top: "4vw",
+                WebkitOverflowScrolling: "touch"
+              }
+            }}
+          >
+            <button onClick={this.closeDialog} aria-label="close">X</button>
+
+            <h2>Translation</h2>
+            <p>{this.state.translatedText}</p>
+
+            <h2>Original text</h2>
+            <p>{this.state.originalText}</p>
+          </ReactModal>
         </div>
-      ))}
-    </div>
 
-    <style global jsx>{`
-      .comic-page {
-        margin: 0 auto;
-        max-width: 600px;
-        padding: ${baseline} 20px;
-      }
+        <style global jsx>{`
+          .comic-page {
+            margin: 0 auto;
+            max-width: 600px;
+            padding: ${baseline} 20px;
+          }
 
-      .comic-page > h1 {
-        font-size: 30px;
-        font-weight: bold;
-        margin-bottom: ${baseline * 1}px;
-        margin-top: ${baseline * 2}px;
-      }
+          .comic-page > h1 {
+            font-size: 30px;
+            font-weight: bold;
+            margin-bottom: ${baseline * 1}px;
+            margin-top: ${baseline * 2}px;
+          }
 
-      .comic-page__panel {
-        margin-top: ${baseline * 3}px;
-      }
+          .comic-page__panel {
+            margin-top: ${baseline * 3}px;
+          }
 
-      .comic-page__bubble {
-        margin-bottom: ${baseline}px;
-      }
-    `}</style>
-  </Main>
-);
+          .comic-page__bubble {
+            margin-bottom: ${baseline}px;
+          }
+        `}</style>
+      </Main>
+    );
+  }
+}
 
 export default Comic;
--- server.js
+++ server.js
@@ -2,15 +2,45 @@
 
 const express = require("express");
 const next = require("next");
+const nconf = require("nconf");
+const MsTranslator = require("mstranslator");
 
 const dev = process.env.NODE_ENV !== "production";
+
+// Setup nconf to use (in-order):
+//   1. Command-line arguments
+//   2. Environment variables
+//   3. A file located at 'config.json'
+nconf.argv().env().file({ file: "./config.json" });
+
 const app = next({ dev });
 const handle = app.getRequestHandler();
+
+var msTranslatorClient = new MsTranslator(
+  {
+    api_key: nconf.get("MSTRANSLATOR_API_KEY")
+  },
+  true
+);
 
 app
   .prepare()
   .then(() => {
     const server = express();
+
+    server.get("/api/translate/:text", (req, res) => {
+      const params = {
+        text: req.params.text,
+        from: "fr",
+        to: "en"
+      };
+
+      msTranslatorClient.translate(params, (err, translation) => {
+        if (err) throw err;
+
+        res.send(JSON.stringify({ translation: translation }));
+      });
+    });
 
     server.get("*", (req, res) => {
       return handle(req, res);
